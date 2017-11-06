e01ee4de603af4d51c3c2e0321c02200fa7e6092
xzvfinet <xzvfinet@gmail.com>
2017-11-06 21:40:06 +0900

    Added random color for labels

--- src/pMain.js
+++ src/pMain.js
@@ -1,10 +1,25 @@
-var path;
+var colors = [
+    new Color(getRandomColor(0.5)),
+    new Color(getRandomColor(0.5)),
+    new Color(getRandomColor(0.5)),
+    new Color(getRandomColor(0.5))
+];
 
-// var textItem = new PointText({
-//     content: 'Click and drag to draw a line.',
-//     point: new Point(20, 30),
-//     fillColor: 'black',
-// });
+for (var i = 0; i < colors.length; ++i) {
+    var textItem = new PointText({
+        content: 'Label' + i + ':',
+        point: new Point(20, 30 + 20 * i),
+        fillColor: 'black',
+    });
+    var pa = new Path.Rectangle({
+        center: [70, 25 + 20 * i],
+        size: [10, 10],
+        fillColor: colors[i] + new Color(0,0,0,0.5)
+    });
+}
+
+var currentPath;
+var currentColor = colors[0];
 
 var hitOptions = {
     segments: true,
@@ -16,39 +31,47 @@ var hitOptions = {
 function onMouseDown(event) {
     var hitResult = project.hitTest(event.point, hitOptions);
     if (!hitResult) {
-
-        if (path) {
-            path.selected = false;
+        if (currentPath) {
+            currentPath.selected = false;
         }
 
-        path = new Path({
+		// new Color(currentColor) - new Color(0,0,0,0.5);
+        currentPath = new Path({
             segments: [event.point],
-            strokeColor: 'red',
+            strokeColor: currentColor + new Color(0,0,0,0.2),
             strokeWidth: 5,
             fullySelected: true
         });
 
-        path.selected = false;
-
+        currentPath.selected = false;
+        currentPath.selectable = true;
 
         return;
+    } else if (hitResult.item.selectable) {
+        if (currentPath) {
+            currentPath.selected = false;
+        }
+        currentPath = hitResult.item;
+        currentPath.selected = true;
     } else {
-        path.selected = false;
-        path = hitResult.item;
-        path.selected = true;
+        if (currentPath) {
+            currentPath.selected = false;
+        }
+        currentPath = null;
+
+        currentColor = hitResult.item.fillColor - new Color(0,0,0,0.5);
     }
-
-
 }
 
 // While the user drags the mouse, points are added to the path
 // at the position of the mouse:
 function onMouseDrag(event) {
-    if (!path.selected) {
-        path.add(event.point);
-    } else {
-        console.log(event.delta);
-        path.position += event.delta;
+    if (currentPath) {
+        if (!currentPath.selected) {
+            currentPath.add(event.point);
+        } else {
+            currentPath.position += event.delta;
+        }
     }
 }
 
@@ -61,13 +84,15 @@ function onMouseUp(event) {
 
     // // Select the path, so we can see its segments:
     // // path.fullySelected = true;
-    if (!path.selected) {
-        path.closed = true;
-        path.selected = true;
-        path.fillColor = new Color(1, 0, 0, 0.5);
+    if (currentPath) {
+        if (!currentPath.selected) {
+            currentPath.closed = true;
+            currentPath.selected = true;
+            currentPath.fillColor = currentColor;
 
-        if (path.segments.length == 1) {
-            path.remove();
+            if (currentPath.segments.length == 1) {
+                currentPath.remove();
+            }
         }
     }
 
@@ -81,25 +106,25 @@ function onMouseUp(event) {
 
 $(document).keydown(function(e) {
     // var offset = $drag_drop.offset();
-    if (path && path.selected) {
+    if (currentPath && currentPath.selected) {
         switch (e.which) {
             case 37: // left
-                path.position += new Point(-5, 0);
+                currentPath.position += new Point(-5, 0);
                 break;
             case 38: // up
-                path.position += new Point(0, -5);
+                currentPath.position += new Point(0, -5);
                 break;
 
             case 39: // right
-                path.position += new Point(5, 0);
+                currentPath.position += new Point(5, 0);
                 break;
 
             case 40: // down
-                path.position += new Point(0, 5);
+                currentPath.position += new Point(0, 5);
                 break;
             case 46:
-                if (path) {
-                    path.remove();
+                if (currentPath) {
+                    currentPath.remove();
                 }
                 break;
 
@@ -107,8 +132,7 @@ $(document).keydown(function(e) {
                 return; // exit this handler for other keys
         }
     }
-
-    e.preventDefault(); // prevent the default action (scroll / move caret)
+    // e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
 function downloadDataUri(options) {
@@ -121,7 +145,19 @@ function downloadDataUri(options) {
 }
 
 $('#export-button').click(function() {
+    console.log(project._children[0]._children);
+    project._children[0]._children.forEach(function (el, i) {
+    	if (el.constructor == Path && el.selectable) {
+    		el.fillColor.alpha = 1;
+    		el.strokeColor.alpha = 1;
+    	}
+    	else {
+    		el.visible = false;
+    	}
+    });
+
     var svg = project.exportSVG({ asString: true });
+
     downloadDataUri({
         data: 'data:image/svg+xml;base64,' + btoa(svg),
         filename: 'export.svg'
---
+++ src/utility.js
@@ -0,0 +1,3 @@
+function getRandomColor(alpha = 1) {
+	return [Math.random(), Math.random(), Math.random(), alpha];
+}
